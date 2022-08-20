18af3794cf780d44cd0afee8583257588a742177
di.masta <d.simurzin@yandex.ru>
2022-08-21 02:21:07 +0300

    Решил первое задание (2)

--- Lesson1/Task6/Program.cs
+++ Lesson1/Task6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число и нажмите Enter ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число и нажмите Enter ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Число {0} больше числа {1}", a, b);
+}
+else
+{
+    Console.WriteLine("Число {1} больше числа {0}", a, b);
+}
