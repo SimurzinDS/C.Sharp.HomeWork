3ac9ac6722f2738a389156cf661b5a953acbc57f
di.masta <d.simurzin@yandex.ru>
2022-08-21 01:24:58 +0300

    Решил задачу на вывод чисел в диапазоне от -N до N

--- Lesson1/Task4/Program.cs
+++ Lesson1/Task4/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
- 
+﻿Console.WriteLine("Введите число от N и нажмите Enter");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = -N;
+
+
+while (a < N+1)
+{
+    Console.Write("{0} ", a);
+    a++;
+}
