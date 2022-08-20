d384911f123a3541dc35da28db752987d3707112
di.masta <d.simurzin@yandex.ru>
2022-08-20 23:53:38 +0300

    Первая задача для групы

--- Lesson1/Task2/Program.cs
+++ Lesson1/Task2/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Введите число и нажмите Enter ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число и нажмите Enter ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = b * b;
+
+if (c == a)
+{
+    Console.WriteLine("Число {0} являеться квадратом числа {1}", a, b);
+}
+else
+{
+    Console.WriteLine("Число {0} не являеться квадратом числа {1}", a, b);
+}
