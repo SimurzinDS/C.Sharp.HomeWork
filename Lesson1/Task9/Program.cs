fd00e0dac605f256e0a20572a24be0fb580b4472
di.masta <d.simurzin@yandex.ru>
2022-08-21 03:34:55 +0300

    Решил 4 задачу(8)

--- Lesson1/Task9/Program.cs
+++ Lesson1/Task9/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Введите число и нажмите Enter ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i < N+1)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write("{0} ", i);
+    }
+    i++;
+}
