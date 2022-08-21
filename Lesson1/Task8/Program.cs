369882ccf77fbcb988e96d29a43d762a18226d3c
di.masta <d.simurzin@yandex.ru>
2022-08-21 03:22:41 +0300

    Решил дз №3 (6)

--- Lesson1/Task8/Program.cs
+++ Lesson1/Task8/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Введите число и нажмите Enter ");
+int evenNumber = Convert.ToInt32(Console.ReadLine());
+
+if (evenNumber % 2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число нечётное");
+}
