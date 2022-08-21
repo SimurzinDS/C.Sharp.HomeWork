5808ac63f15e962e18428e0c02af508dd2d248ec
di.masta <d.simurzin@yandex.ru>
2022-08-21 03:07:46 +0300

    Решил 2ю задачу (4)

--- Lesson1/Task7/Program.cs
+++ Lesson1/Task7/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿Console.WriteLine("Введите число A и нажмите Enter ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B и нажмите Enter ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C и нажмите Enter ");
+int C = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (A > B)
+{
+    if (A > C)
+    {
+        max = A;
+    }
+    else 
+    {
+        max = C;
+    }
+}
+else
+{
+    if (B > C)
+    {
+        max = B;
+    }
+    else
+    {
+        max = C;
+    }
+}
+Console.WriteLine("Max = {0} ", max);
