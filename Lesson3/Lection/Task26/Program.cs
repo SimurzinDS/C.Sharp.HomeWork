b41659759650ae5b62a71e52b590ec311ab924ff
di.masta <d.simurzin@yandex.ru>
2022-09-05 02:06:13 +0300

    Решил задачу 26

--- Lesson3/Lection/Task26/Program.cs
+++ Lesson3/Lection/Task26/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿// Напишите программу, которая, принимает на вход число и 
+// выдает количество цифр в числе.
+
+// Вариант 1
+
+bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+
+if(isNumber != true)
+{
+    Console.WriteLine("Введите число");
+    return;
+}
+
+int GetDigitsNumber(int arg)
+{
+    string str = Convert.ToString(arg);
+    int result = str.Length;
+    return result;
+}
+
+int digitsNumber = GetDigitsNumber(number);
+Console.WriteLine(digitsNumber);
