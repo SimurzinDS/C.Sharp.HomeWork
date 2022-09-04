cf9d2dc1d92d9892a71c6586dd624087973eb98c
di.masta <d.simurzin@yandex.ru>
2022-09-05 02:27:58 +0300

    Решил 28 задачу

--- Lesson3/Lection/Task28/Program.cs
+++ Lesson3/Lection/Task28/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Задача 28:
+// Напишите программу, которая принимает на вход значение 
+// число N и выдает произведение чисел от 1 до N
+
+bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+
+int GetFactorial(int arg)
+{
+    int result = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+int Sum = GetFactorial(number);
+Console.WriteLine(Sum);
