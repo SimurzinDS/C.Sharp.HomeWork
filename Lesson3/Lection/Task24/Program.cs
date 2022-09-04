1729e3126e12d5c651f88c3728fb74e9f86cf727
di.masta <d.simurzin@yandex.ru>
2022-09-05 00:47:27 +0300

    Решили задачу

--- Lesson3/Lection/Task24/Program.cs
+++ Lesson3/Lection/Task24/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+
+if (isNumber == false)
+{
+    Console.WriteLine("Is not number");
+    return;
+}
+
+int GetSumNumber( int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int result = GetSumNumber(number);
+Console.WriteLine(result);
