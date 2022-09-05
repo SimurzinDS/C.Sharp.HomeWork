24b993479b91b3207c65f41ed96218c0ef693bdc
di.masta <d.simurzin@yandex.ru>
2022-09-06 01:32:12 +0300

    Решил задачу 32

---
+++ Lesson5/Lection/Task32/Program.cs
@@ -0,0 +1,41 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-10, 10);
+        index++;
+    }
+}
+
+void PrintArray( int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+int [] SingReplacement(int[] arr)
+{
+    int count = arr.Length;
+    int index = 0;
+    int a = -1;
+    while (index < count)
+    {
+        arr[index] = arr[index] * a;
+        index++;
+    }
+    return arr;
+}
+
+int [] array = new int[new Random().Next(0, 10)];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(SingReplacement(array));
