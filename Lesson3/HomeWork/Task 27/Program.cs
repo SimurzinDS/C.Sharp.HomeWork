5c81a4f6663932ef5e84b511d057d593906fa86f
di.masta <d.simurzin@yandex.ru>
2022-09-05 12:38:28 +0300

    Решил 27 задачу

--- Lesson3/HomeWork/Task 27/Program.cs
+++ Lesson3/HomeWork/Task 27/Program.cs
@@ -6,3 +6,26 @@
 
 // 9012 -> 12
 
+Console.WriteLine("Введите число и нажмите Enter");
+bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+
+if (!isNumber)
+{
+    Console.WriteLine("Не является числом");
+    return;
+}
+
+int GetSumNumber(int arg)
+{
+    int sum = 0;
+    while (arg > 0)
+    {
+        sum = sum + arg % 10;
+        arg = arg / 10;
+    }
+    return sum;
+}
+
+int result = GetSumNumber(number);
+Console.WriteLine(result);
+
