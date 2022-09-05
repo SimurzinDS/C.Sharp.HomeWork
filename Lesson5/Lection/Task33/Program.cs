adcc675e94750574f2b4dd41acbd51d172a03510
di.masta <d.simurzin@yandex.ru>
2022-09-06 01:50:22 +0300

    Решил задачу 33

---
+++ Lesson5/Lection/Task33/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.WriteLine("Введите искомое число в массиве: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray( int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+string FindNumber(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while(index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            return "Да";
+        }
+        index++;
+    }
+    return "Нет";
+}
+
+int [] array = new int[new Random().Next(5, 10)];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+string pos = FindNumber(array, a);
+Console.WriteLine(pos);
+
