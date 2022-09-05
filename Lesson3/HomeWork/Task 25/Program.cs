f035a357beea7818794dc8e7c436a561ad8b8d23
di.masta <d.simurzin@yandex.ru>
2022-09-05 04:26:36 +0300

    Решил 25 задачу

--- Lesson3/HomeWork/Task 25/Program.cs
+++ Lesson3/HomeWork/Task 25/Program.cs
@@ -4,3 +4,32 @@
 
 // 2, 4 -> 16
 
+bool firstNumber = int.TryParse(Console.ReadLine(), out int number1);
+bool secondNumber = int.TryParse(Console.ReadLine(), out int number2);
+
+if (!firstNumber || !secondNumber)
+{
+    Console.WriteLine("Одно из чисел не является числом");
+}
+
+
+
+int GetMathPow(int arg1, int arg2)
+{
+    int result = 1;
+    if (arg2 != 0)
+    {
+        for (int i = 0; i < arg2; i++)
+        {
+            result = result * arg1;
+        }
+    }
+    else
+    {
+        return 1;
+    }
+    return result;
+}
+
+int squareNumber = GetMathPow(number1, number2);
+Console.WriteLine(squareNumber);
