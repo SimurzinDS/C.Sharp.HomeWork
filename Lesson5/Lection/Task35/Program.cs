fc8dd88c89d8938762002df302f2f6c239b883b4
di.masta <d.simurzin@yandex.ru>
2022-09-08 13:36:26 +0300

    Решил 35 задачу

---
+++ Lesson5/Lection/Task35/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 124);
+        index++;
+    }
+}
+
+void PrintArray( int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+int FindNumber(int[] collection)
+{
+    int count = collection.Length;
+    int index = 0;
+    int count2 = 0;
+
+    while(index < count)
+    {
+        if (collection[index] > 9 && collection[index] < 100)
+        {
+            count2++;
+        }
+        index++;
+    }
+    return count2;
+}
+
+int [] array = new int[5];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = FindNumber(array);
+Console.WriteLine(pos);
