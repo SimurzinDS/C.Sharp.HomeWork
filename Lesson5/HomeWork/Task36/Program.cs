a8f691410bec429451f203958c2ceca3c472c15d
di.masta <d.simurzin@yandex.ru>
2022-09-10 16:21:04 +0300

    Исправил 36 задачу с учетом комментариев

--- Lesson5/HomeWork/Task36/Program.cs
+++ Lesson5/HomeWork/Task36/Program.cs
@@ -1,30 +1,55 @@
-﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-Console.WriteLine($"\nЗадача 36. Сумма элементов, стоящих на нечётных позициях:\n");
-Console.Write($"Введи количество элементов массива: ");
-int numberElements = Convert.ToInt32(Console.ReadLine()); 
-
-int RandomNumbers(int numberElements, int min, int max)
-  {
-  int[] randomNumbers = new int[numberElements];
-  int sumElements = 0;
-  Console.Write("Получившийся массив: ");
-
-    for (int i = 0; i <randomNumbers.Length; i++ ){
-      randomNumbers[i] = new Random().Next(min, max);
-
-      Console.Write(randomNumbers[i] + " ");
-
-      if (i % 2 != 1)
-      {
-        sumElements = sumElements + randomNumbers[i];
-      }
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-99, 100);
+        index++;
     }
-  return sumElements;
-  }
+}
 
-int randomNumbers =  RandomNumbers(numberElements, 1, 10);
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    Console.Write("[");
+    while (position < count)
+    {
+        if (position + 1  != count)
+        {
+            Console.Write($"{col[position]}, ");
+        }
+        else
+        {
+            Console.Write($"{col[position]}");
+        }
+        position++;
+    }
+    Console.Write("]");
+}
 
-Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomNumbers}");
+int [] array = new int[new Random().Next(4, 10)];
+FillArray(array);
+PrintArray(array);
+
+int GetSumOddNumbers(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+          sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+int result = GetSumOddNumbers(array);
+Console.Write($" -> {result}");
