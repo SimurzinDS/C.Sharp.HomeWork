01d3f393406b48bbbeb872d098d70360d6792888
di.masta <d.simurzin@yandex.ru>
2022-09-13 14:50:23 +0300

    Решил 42 задачу

---
+++ Lesson6/Lection/Task42/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите десятичное число");
+bool isNumber = int.TryParse(Console.ReadLine(), out int x);
+
+if(isNumber == false)
+{
+    Console.WriteLine($"это не десятичное число!");
+}
+
+string GetBinareNumber(int arg)
+{
+    int y = 0;
+    string result = String.Empty;
+    
+    while(arg > 0)
+    {
+        y = arg % 2;
+        result = Convert.ToString(y) + result;
+        arg = arg / 2;
+    }
+    return result;
+}
+
+string binareNumber = GetBinareNumber(x);
+Console.WriteLine(binareNumber);
