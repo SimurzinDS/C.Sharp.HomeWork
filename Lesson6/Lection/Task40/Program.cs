c880e44c93ab7a56489db67d8f07663afc79c1e5
di.masta <d.simurzin@yandex.ru>
2022-09-13 14:13:45 +0300

    Решил задачу 40

---
+++ Lesson6/Lection/Task40/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите 3 числа через Enter");
+bool isNumber1 = int.TryParse(Console.ReadLine(), out int a);
+bool isNumber2 = int.TryParse(Console.ReadLine(), out int b);
+bool isNumber3 = int.TryParse(Console.ReadLine(), out int c);
+
+if(isNumber1 != true || isNumber2 != true || isNumber3 != true)
+{
+    Console.WriteLine("Одно из введенных параметров не число!");
+}
+
+bool GetToBeOrNotToBe(int x, int y, int z)
+{
+    if(x <= 0 || y <= 0 || z <= 0)
+    {
+        return false;
+    }
+    if(x < y+z && y < x + z && z < x + y)
+    {
+        return true;
+    }
+    else return false;
+}
+
+bool result = GetToBeOrNotToBe(a, b, c);
+Console.WriteLine(result);
