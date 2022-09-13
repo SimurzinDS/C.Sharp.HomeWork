76dea5da3ab021142f97de8b8fd33f01320f5573
di.masta <d.simurzin@yandex.ru>
2022-09-13 15:17:32 +0300

    Решил 44 задачу без рекурсии

---
+++ Lesson6/Lection/Task44/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите число N");
+bool isNumber = int.TryParse(Console.ReadLine(), out int n);
+
+if(isNumber != true)
+{
+    Console.WriteLine("Это не число");
+    return;
+}
+
+string Fibonacci(int arg)
+{
+    int a = 0;
+    int b = 1;
+    int c = 0;
+    string subsequence = String.Empty;
+    for (int i = 0; i < arg; i++)
+    {
+        subsequence = subsequence + Convert.ToString(a) + " ";
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return subsequence;
+}
+
+string result = Fibonacci(n);
+Console.WriteLine(result);
